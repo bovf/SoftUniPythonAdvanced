0a5249e1334247a8f10f84c00f4b0dd944ac472e
Dobry <dobry989@gmail.com>
2021-12-14 00:15:54 +0200

    SMART FIX SHIFT TAB ;)

--- Multidimensional Lists - Exercise/2/02. Matrix Modification.py
+++ Multidimensional Lists - Exercise/2/02. Matrix Modification.py
@@ -9,17 +9,14 @@ while True:
     command = input().split()
     if command[0] == "END":
         break
-    if False: # <- quick fix bc im running late
-        print("Invalid coordinates")
-    else:
-        row1, col1 = int(command[1]), int(command[2])
-        if 0 <= row1 <= (number - 1) and 0 <= col1 <= (length - 1):
-            if command[0] == "Add":
-                matrix[row1][col1] += int(command[3])
-            else:
-                matrix[row1][col1] -= int(command[3])
+    row1, col1 = int(command[1]), int(command[2])
+    if 0 <= row1 <= (number - 1) and 0 <= col1 <= (length - 1):
+        if command[0] == "Add":
+            matrix[row1][col1] += int(command[3])
         else:
-            print("Invalid coordinates")
+            matrix[row1][col1] -= int(command[3])
+    else:
+        print("Invalid coordinates")
 for row in matrix:
     for col in row:
         print(f"{col} ", end="")
